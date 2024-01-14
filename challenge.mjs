547ad55ec96575b09f89ea5a6a411bda216afa44
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-01-15 00:25:48 +0600

    Fill the code

--- challenge.mjs
+++ challenge.mjs
@@ -1,12 +1,47 @@
 const weirdExpressions = [
-  "a othoba b",
-  "b ebong c othoba d",
-  "ebong ebong othoba othoba ebong",
-  "((ebong) othoba ebong) ebong othoba",
-  "(ebong othoba (ebong ebong ((othoba)othoba(ebong))))",
-  "ebong",
+    "a othoba b",
+    "b ebong c othoba d",
+    "ebong ebong othoba othoba ebong",
+    "((ebong) othoba ebong) ebong othoba",
+    "(ebong othoba (ebong ebong ((othoba)othoba(ebong))))",
+    "ebong",
 ];
 
 for (const expression of weirdExpressions) {
-  // fill this in
+    // fill this in
+    const exp = expression.split(" ");
+
+    for (let i = 0; i < exp.length; i++) {
+        if (i === 0 && exp[i] === "ebong") {
+            continue;
+        }
+        if (i === 0 && exp[i] === "othoba") {
+            continue;
+        }
+        if (i !== 0 && exp[i] === "ebong" && exp[i - 1] === "&&") {
+            continue;
+        }
+        if (i !== 0 && exp[i] === "ebong" && exp[i - 1] === "||") {
+            continue;
+        }
+        if (i !== 0 && exp[i] === "othoba" && exp[i - 1] === "&&") {
+            continue;
+        }
+        if (i !== 0 && exp[i] === "othoba" && exp[i - 1] === "||") {
+            continue;
+        }
+        if (exp[i] !== "ebong" && exp[i] !== "othoba") {
+            exp[i] = exp[i].replace(")ebong", ")&&");
+            exp[i] = exp[i].replace(")othoba", ")||");
+            continue;
+        }
+        if (exp.length > 1 && exp[i] === "ebong") {
+            exp[i] = "&&";
+        }
+        if (exp.length > 1 && exp[i] === "othoba") {
+            exp[i] = "||";
+        }
+    }
+
+    console.log(exp.join(" "));
 }
